1c13964a64ceb68399d923c5b30b295410e5fca5
weAllHateMicrosoft <c1seamoon@gmail.com>
2026-03-24 12:00:30 -0400

    Create Karaoke.java

---
+++ Karaoke.java
@@ -0,0 +1,47 @@
+public class Karaoke {
+    public static void main(String[] args) throws InterruptedException {
+        
+        String name = System.console().readLine("What is your name, superstar? ");
+
+        System.out.println("\nAlright " + name + ", let's take it to the bridge!");
+        Thread.sleep(1500); // Pause for 1.5 seconds
+        System.out.print("You are...");
+        Thread.sleep(1000); // Pause for 1 second
+        System.out.println("my fire...");
+        Thread.sleep(1000);
+        System.out.print("The one...");
+        Thread.sleep(1000);
+        System.out.println("desire...");
+        Thread.sleep(2000);
+        System.out.println("Believe...");
+        Thread.sleep(1000); 
+        System.out.println("when I say---");
+        Thread.sleep(1000);
+        System.out.println("I");
+        Thread.sleep(1000);
+        System.out.println("WANT");
+        Thread.sleep(1000);
+        System.out.println("IT");
+        Thread.sleep(1000);
+        System.out.println("THAT");
+        Thread.sleep(1000);
+        System.out.println("WAY");
+        Thread.sleep(1000);
+
+        System.console().readLine("\n(Press Enter to scream the chorus!)");
+
+        System.out.println("TELL ME WHY!!");
+        Thread.sleep(800);
+        System.out.println("Ain't nothin' but a heartache...");
+        Thread.sleep(800);
+        System.out.println("TELL ME WHY!!");
+        Thread.sleep(800);
+        System.out.println("Ain't nothin' but a mistake...");        
+        Thread.sleep(1500);
+        System.out.println("Tell me why~");
+        Thread.sleep(800);
+        System.out.println("I never want to hear " + name + " say...");
+        Thread.sleep(2000);
+        System.out.println("I WANT IT THAT WAY!");
+    }
+}
